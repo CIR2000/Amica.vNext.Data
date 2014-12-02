033b8ef6ed61ccf2c7f1c539f1a597551a731e35
Nicola Iarocci <nicola@nicolaiarocci.com>
2014-12-02 18:59:53 +0100

    first commit

---
+++ Amica.vNext.Data.SQLite/Database.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using SQLite.Net;
+using Amica.vNext.Objects;
+
+namespace Amica.vNext.Data.SQLite
+{
+	public class Database : IDatabase
+	{
+		static object locker = new object ();
+		SQLiteConnection database;
+
+		public Database (SQLiteConnection connection)
+		{
+			database = connection;
+
+			// create the tables
+			database.CreateTable<Company> ();
+		}
+
+		public List<Company> GetCompanies ()
+		{
+			lock (locker) {
+				return (from i in database.Table<Company> ()
+				        select i).ToList ();
+			}
+		}
+	}
+}
+
---
+++ IDatabase.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using Amica.vNext.Objects;
+
+namespace Amica.vNext.Data
+{
+	public interface IDatabase
+	{
+		List<Company> GetCompanies ();
+	}
+}
+

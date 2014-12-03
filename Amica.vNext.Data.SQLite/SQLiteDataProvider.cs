3710d91b8d7016d508134e95e7f361445c9c79b4
Nicola Iarocci <nicola@nicolaiarocci.com>
2014-12-03 15:10:08 +0100

    Syncronous interface implementation for SQLite

--- Amica.vNext.Data.SQLite/SQLiteDataProvider.cs
+++ Amica.vNext.Data.SQLite/SQLiteDataProvider.cs
@@ -19,11 +19,46 @@ namespace Amica.vNext.Data.SQLite
 			_database.CreateTable<Company> ();
 		}
 
-		public List<Company> GetCompanies ()
+		public List<T> GetItems<T> () where T: new()
 		{
 			lock (Locker) {
-				return (from i in _database.Table<Company> ()
+				return (from i in _database.Table<T> ()
 				        select i).ToList ();
+			}
+		}
+
+		public T GetItem<T> (object id) where T: new()
+		{
+			lock (Locker) {
+				return _database.Get<T> (id);
+			}
+		}
+
+		public void Update (object item)
+		{
+			lock (Locker) {
+				_database.Update (item);
+			}
+		}
+
+		public void Insert (object item)
+		{
+			lock (Locker) {
+				_database.Insert (item);
+			}
+		}
+
+		public void Delete (object item)
+		{
+			lock (Locker) {
+				_database.Delete (item);
+			}
+		}
+
+		public void Delete<T> (object id)
+		{
+			lock (Locker) {
+				_database.Delete<T> (id);
 			}
 		}
 	}
